9be171f0000dab6d2b55d6eb34dffb3a19d65926
lisp3r <lisp3r@github.com>
2023-04-02 21:28:44 +0600

    Update simple_app_2

--- studying/simple_app_2/README.md
+++ studying/simple_app_2/README.md
@@ -1,3 +1,55 @@
 # Simple App 2
 
 Same as [Simple App 1](../simple_app_1/) but it waits for pressing the back button to exit instead of `furi_delay(2000)`.
+
+## Events
+
+```c
+static void input_callback(InputEvent* input_event, void* ctx) {
+    // check that the context is not null
+    furi_assert(ctx);
+    FuriMessageQueue* event_queue = ctx;
+
+    // just put the new event in the query we have here
+    furi_message_queue_put(event_queue, input_event, FuriWaitForever);
+}
+
+typedef struct {
+    ...
+    FuriMessageQueue* event_queue;
+} App;
+
+App* app_alloc() {
+    App* app = malloc(sizeof(App));
+    ...
+    view_port_input_callback_set(app->view_port, input_callback, app->event_queue);
+    ...
+}
+
+void app_free(App* app) {
+    ...
+    furi_message_queue_free(app->event_queue);
+    ...
+}
+
+int32_t simple_app_2_main(void* p) {
+    UNUSED(p);
+
+    App* app = app_alloc();
+
+    InputEvent event;
+    while(1) {
+        // Выбираем событие из очереди в переменную event (ждем бесконечно долго, если очередь пуста)
+        // и проверяем, что у нас получилось это сделать
+        furi_check(furi_message_queue_get(app->event_queue, &event, FuriWaitForever) == FuriStatusOk);
+
+        if(event.key == InputKeyBack) {
+            break;
+        }
+    }
+    ...
+```
+
+![screenshot1](Screenshot-20230402-193400.png)
+
+![screenshot2](DeepinScreenshot_select-area_20230402210843.png)
--- studying/simple_app_2/simple_app_2.c
+++ studying/simple_app_2/simple_app_2.c
@@ -2,10 +2,65 @@
 #include <gui/gui.h>
 #include <input/input.h>
 #include <notification/notification_messages.h>
+#include <string.h>
 
 #include "simple_app_2_icons.h"
 
 #define TAG "simple_app_2"
+
+
+static void log_events(InputEvent* input_event) {
+
+    char* evt_key = "UnknownEvent";
+    char* evt_type = "UnknownType";
+
+    switch(input_event->key) {
+        case InputKeyBack:
+            evt_key = "InputKeyBack";
+            break;
+        case InputKeyUp:
+            evt_key = "InputKeyUp";
+            break;
+        case InputKeyDown:
+            evt_key = "InputKeyDown";
+            break;
+        case InputKeyRight:
+            evt_key = "InputKeyRight";
+            break;
+        case InputKeyLeft:
+            evt_key = "InputKeyLeft";
+            break;
+        case InputKeyOk:
+            evt_key = "InputKeyOk";
+            break;
+        case InputKeyMAX:
+            evt_key = "InputKeyMAX";
+            break;
+    }
+
+    switch(input_event->type) {
+        case InputTypePress:
+            evt_type = "InputTypePress";
+            break;
+        case InputTypeRelease:
+            evt_type = "InputTypeRelease";
+            break;
+        case InputTypeShort:
+            evt_type = "InputTypeShort";
+            break;
+        case InputTypeLong:
+            evt_type = "InputTypeLong";
+            break;
+        case InputTypeRepeat:
+            evt_type = "InputTypeRepeat";
+            break;
+        case InputTypeMAX:
+            evt_type = "InputTypeMAX";
+            break;
+    }
+
+    FURI_LOG_I(TAG, "New event: %s (%s)", evt_key, evt_type);
+}
 
 
 typedef struct {
@@ -29,6 +84,8 @@ static void input_callback(InputEvent* input_event, void* ctx) {
     // check that the context is not null
     furi_assert(ctx);
     FuriMessageQueue* event_queue = ctx;
+
+    log_events(input_event);
 
     furi_message_queue_put(event_queue, input_event, FuriWaitForever);
 }
@@ -64,11 +121,8 @@ int32_t simple_app_2_main(void* p) {
     App* app = app_alloc();
 
     InputEvent event;
-    // FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
 
     while(1) {
-        // Выбираем событие из очереди в переменную event (ждем бесконечно долго, если очередь пуста)
-        // и проверяем, что у нас получилось это сделать
         furi_check(furi_message_queue_get(app->event_queue, &event, FuriWaitForever) == FuriStatusOk);
 
         if(event.key == InputKeyBack) {
