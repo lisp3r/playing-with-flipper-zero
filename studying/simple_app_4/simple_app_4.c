169991fed14126f2c2caaa08fcb7db06a9e7ef79
lisp3r <lisp3r@github.com>
2023-08-20 19:38:27 +0300

    Add Simple App 4: WIP

--- studying/simple_app_4/simple_app_4.c
+++ studying/simple_app_4/simple_app_4.c
@@ -172,16 +172,6 @@ int32_t simple_app_4_main(void* p) {
         }
     }
 
-    // From storage.h:
-    //
-    // #define STORAGE_APP_ASSETS_PATH_PREFIX "/assets"
-    //
-    // #define APP_ASSETS_PATH(path) STORAGE_APP_ASSETS_PATH_PREFIX "/" path
-    // example_apps_data_print_file_content(storage, APP_ASSETS_PATH("test_asset.txt"));
-    // example_apps_data_print_file_content(storage, APP_ASSETS_PATH("poems/a jelly-fish.txt"));
-    // example_apps_data_print_file_content(storage, APP_ASSETS_PATH("poems/theme in yellow.txt"));
-    // example_apps_data_print_file_content(storage, APP_ASSETS_PATH("poems/my shadow.txt"));
-
     app_free(app);
     return 0;
 }
